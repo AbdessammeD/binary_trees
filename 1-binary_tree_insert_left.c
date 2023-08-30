7ed339592f8298e069c5d72216431f8171c23d63
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-08-30 06:20:23 -0700

    binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		node = binary_tree_node(parent, value);
+		if (!node)
+			return (NULL);
 		parent->left = node;
 	}
 	return (node);
